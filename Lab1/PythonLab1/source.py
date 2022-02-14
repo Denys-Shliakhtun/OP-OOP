bcbd789c95c34ee0096690a6f2e335b99dc5d86b
Denys-Shliakhtun <denys.shlakhtun@gmail.com>
2022-02-14 19:12:13 +0200

    Lab1

---
+++ Lab1/PythonLab1/source.py
@@ -0,0 +1,19 @@
+#Створити текстовий файл. 
+#Сформувати новий текстовий файл, що складається з рядків вихідного файлу, у яких задане користувачем слово. 
+#Визначити розмір, дату і час створення нового файлу. 
+#Вивести вміст вихідного і створеного файлів.
+
+import fileLib #включення користувацької бібліотеки
+
+word = input("Enter the word: ")                        #слово для виконання поставленої задачі
+fileInName = input("Enter the name of input file: ")    #введення назви файлу ввідних даних
+fileOutName = 'output.txt'                              #назва файлу вихідних даних
+fileLib.fileInput(fileInName, "wt")                     #введення даних у файл ввідних даних
+fileLib.fileGen(fileInName, fileOutName, word)          #створення файлу вихідних даних
+fileLib.fileOutput(fileInName)                          #виведення файлу ввідних даних
+fileLib.fileOutput(fileOutName)                         #виведення файлу вихідних даних
+fileLib.fileStat(fileOutName)                           #відображення статистики файлу
+while fileLib.fileAdd(fileInName):                      #поки користувач бажає додати дані до файлу ввідних даних
+    fileLib.fileGen(fileInName, fileOutName, word)          #створення файлу вихідних даних
+    fileLib.fileOutput(fileInName)                          #виведення файлу ввідних даних
+    fileLib.fileOutput(fileOutName)                         #виведення файлу вихідних даних
