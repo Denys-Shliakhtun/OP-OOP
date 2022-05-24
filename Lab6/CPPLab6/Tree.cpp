fe662f8b42401e2f141c8ac87b11849f73a31b34
Denys-Shliakhtun <denys.shlakhtun@gmail.com>
2022-05-24 09:44:15 +0300

    Lab6

---
+++ Lab6/CPPLab6/Tree.cpp
@@ -0,0 +1,108 @@
+﻿#include "Tree.h"
+#include <iomanip>
+#include <queue>
+
+//конструктор з параметром
+Node::Node(float value)
+{
+	data = value;
+	left = nullptr;
+	right = nullptr;
+}
+
+//вставлення нової вершини-нащадка
+//pos == 1 -> left, pos == 2 -> right
+Node* Node::insertNodePointer(Node* value, int pos)
+{
+	switch (pos)
+	{
+	case 1:
+		left = value;
+		return left;
+	case 2:
+		right = value;
+		return right;
+	}
+	return nullptr;
+}
+
+//заповнення дерева заданими значеннями масиву
+Node* Tree::createTree(float* arr, int from, int num)
+{
+	if (num == 0)
+		return nullptr;
+	Node* p = new Node(arr[from]);
+	int numleft = num / 2;
+	int numright = num - numleft - 1;
+	p->insertNodePointer(createTree(arr, from + 1, numleft), 1);
+	p->insertNodePointer(createTree(arr, from + 1 + numleft, numright), 2);
+	return p;
+}
+
+//знаходження суми значень вершин дерева
+float Tree::getAverageBFS()
+{
+	std::queue <Node*> plan; //черга для пошуку в ширину
+	Node* temp = root;		 //проміжна змінна
+	plan.push(temp);		 //додання кореню дерева в чергу
+	float sum = 0;			 //сума значень
+	float count = 0;		 //кількість вершин
+	Node* temp2;			 //друга проміжна змінна
+	while (plan.size()>0) //поки в черзі є елементи
+	{
+		//діставання елемента з черги
+		temp = plan.front();
+		plan.pop();
+
+		//додавання лівого нащадка до черги
+		temp2 = temp->getLeftPointer();
+		if (temp2!=nullptr)		
+			plan.push(temp2);		
+
+		//додавання правого нащадка до черги
+		temp2 = temp->getRightPointer();
+		if (temp2 != nullptr)		
+			plan.push(temp2);		
+
+		//додавання значення temp до загальної суми
+		if(temp!=nullptr)
+			sum += temp->getData();
+		
+		count++;
+	}
+	return sum/count;
+}
+
+//генерація масиву дійсних чисел заданого розміру
+float* arrGen(int num)
+{
+	float* arr = new float[num];
+	for (int i = 0; i < num; i++)
+		arr[i] = float(rand() % 100) / 10;
+	return arr;
+}
+
+//виведення дерева
+void treeOutput(const std::string& prefix, Node* node, bool isLeft)
+{
+	if (node != nullptr)
+	{
+		std::cout << prefix;
+
+		std::cout << "'--";
+
+		std::cout << std::fixed << std::setprecision(1) << node->getData() << std::endl;
+
+		treeOutput(prefix + (isLeft ? "|   " : "    "), node->getLeftPointer(), true);
+		treeOutput(prefix + (isLeft ? "|   " : "    "), node->getRightPointer(), false);
+	}
+}
+
+//введення кількості елементів дерева
+int numInput()
+{
+	int num;
+	std::cout << "Enter the number of nodes: ";
+	std::cin >> num;
+	return num;
+}
---
+++ Lab6/CPPLab6/Tree.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <iostream>
+
+struct Node
+{
+	float data;		//дані вершини
+	Node* left;		//посилання на лівого нащадка
+	Node* right;	//посилання на правого нащадка
+public:	
+	//конструктор з параметром
+	Node(float value);
+	
+	//геттери
+	float getData() { return data; };
+	Node* getLeftPointer() { return left; };	
+	Node* getRightPointer() { return right; };
+
+	//вставлення нової вершини-нащадка
+	//pos == 1 -> left, pos == 2 -> right
+	Node* insertNodePointer(Node* value, int pos);
+};
+
+//введення кількості елементів дерева
+int numInput();
+
+//генерація масиву дійсних чисел заданого розміру
+float* arrGen(int num);
+
+//виведення дерева
+void treeOutput(const std::string& prefix, Node* node, bool isLeft);
+
+class Tree
+{
+	Node* root; //корінь дерева
+	
+	//заповнення дерева заданими значеннями масиву
+	Node* createTree(float* arr, int from, int num);
+public:
+	//конструктор за замовчуванням
+	Tree() : Tree(numInput()) {};
+	
+	//конструктор з параметром, заповнення дерева випадковими значеннями
+	Tree(int num) : Tree(arrGen(num), num) {};
+	
+	//конструктор з двома параметрами заповнення дерева
+	Tree(float* arr, int num) { root = createTree(arr, 0, num); };
+	
+	//геттер
+	Node* getRoot() { return root; };
+
+	//знаходження середнього значення вершин дерева
+	float getAverageBFS();
+};
